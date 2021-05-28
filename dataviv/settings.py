f8ead03f2ecaefc20dc99fa2a7901759dd05edc6
testacc93 <84958260+testacc93@users.noreply.github.com>
2021-05-28 16:44:53 +0530

    Update settings.py

--- dataviv/settings.py
+++ dataviv/settings.py
@@ -136,6 +136,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
